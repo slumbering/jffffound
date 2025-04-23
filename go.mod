5a367ccb1e527c50714ab5c6c4b8760d52013b78
slumbering <slumbering.pierrot@gmail.com>
2025-04-23 23:03:40 +0200

    pull github md files and create static files with it

--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module jffffound.com
 
 go 1.24.2
 
-require github.com/yuin/goldmark v1.7.10 // indirect
+require github.com/google/go-github/v71 v71.0.0
+
+require github.com/google/go-querystring v1.1.0 // indirect
--- til.go
+++ til.go
@@ -1,14 +1,14 @@
-//go:build ignore
-
 package main
 
 import (
-	"bytes"
+	"context"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 
-	"github.com/yuin/goldmark"
+	"github.com/google/go-github/v71/github"
 )
 
 type Page struct {
@@ -17,37 +17,73 @@ type Page struct {
 }
 
 func (p *Page) save() error {
-	filename := p.Title + ".txt"
-	return os.WriteFile(filename, p.Body, 0600)
+	filename := p.Title + ".html"
+	return os.WriteFile(filepath.Join("./static", filename), p.Body, 0600)
 }
 
-func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
-	body, err := os.ReadFile(filename)
-	if err != nil {
-		return nil, err
+func getHomePage() *Page {
+	client := github.NewClient(nil).WithAuthToken(os.Getenv("GITHUB_TOKEN"))
+
+	fileContent, _, _, error := client.Repositories.GetContents(context.Background(), "slumbering", "til", "README.md", nil)
+	if error != nil {
+		fmt.Println("Error fetching organizations:", error)
+		return nil
 	}
-	return &Page{Title: title, Body: body}, nil
-}
+	content, err := fileContent.GetContent()
+	if err != nil {
+		fmt.Println("Error getting content:", err)
+		return nil
+	}
+	opt := &github.MarkdownOptions{
+		Mode:    "gfm",
+		Context: "slumbering/til",
+	}
 
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-	source := []byte("# Hello World\nThis is a sample markdown content.")
-	var buf bytes.Buffer
-	if err := goldmark.Convert(source, &buf); err != nil {
-		panic(err)
+	output, _, err := client.Markdown.Render(context.Background(), content, opt)
+	if err != nil {
+		fmt.Println("Error rendering markdown:", err)
+		return nil
 	}
 	p := &Page{
-		Body: buf.Bytes(),
+		Title: "home",
+		Body:  []byte(output),
 	}
-	w.Header().Set("Content-Type", "text/html")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(string(p.Body)))
+
+	p.save()
+
+	return p
 }
 
+// func viewHandler(w http.ResponseWriter, r *http.Request) {
+// 	source := []byte("# Hello World\nThis is a sample markdown content.")
+// 	var buf bytes.Buffer
+// 	if err := goldmark.Convert(source, &buf); err != nil {
+// 		panic(err)
+// 	}
+// 	p := &Page{
+// 		Body: buf.Bytes(),
+// 	}
+// 	w.Header().Set("Content-Type", "text/html")
+// 	w.WriteHeader(http.StatusOK)
+// 	w.Write([]byte(string(p.Body)))
+// }
+
 func main() {
-	http.HandleFunc("/til/", viewHandler)
-	err := http.ListenAndServe(":8080", nil)
+	getHomePage()
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	log.Print("Listening on :3000...")
+	err := http.ListenAndServe(":3000", nil)
+
 	if err != nil {
-		fmt.Println("Error starting server:", err)
+		log.Fatal(err)
 	}
+
+	// http.HandleFunc("/", getHomePage)
+	// http.HandleFunc("/til/", viewHandler)
+	// err := http.ListenAndServe(":8080", nil)
+	// if err != nil {
+	// 	fmt.Println("Error starting server:", err)
+	// }
 }
